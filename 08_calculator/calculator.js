3d916261d45150bbe2a209a7e250ef0dda2449c4
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-11 21:59:44 +0200

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return +a + +b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length) {
+    var sumOfNumbers = 0;
+  } else {
+    return 0;
+  }
+  for (i in array) {
+    sumOfNumbers = +sumOfNumbers + +array[i];
+  }
+  return sumOfNumbers;
 };
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
