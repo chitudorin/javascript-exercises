ec76b44f10296861a07a58929b862a3761bdad1e
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-11 19:00:34 +0200

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (start, finish) {
+  let sum = 0;
+  if (
+    start < 0 ||
+    finish < 0 ||
+    typeof start != "number" ||
+    typeof finish != "number"
+  ) {
+    return "ERROR";
+  } else {
+    if (start > finish) {
+      for (i = start; i >= finish; i--) {
+        sum += i;
+      }
+    } else {
+      for (i = start; i <= finish; i++) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
