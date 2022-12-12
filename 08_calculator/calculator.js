debafab0370bf60cd70472f4c48c9a299cb93282
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-12 20:24:29 +0200

    Complete exercise 08 (full)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -9,20 +9,46 @@ const subtract = function (a, b) {
 const sum = function (array) {
   if (array.length) {
     var sumOfNumbers = 0;
+    for (i in array) {
+      sumOfNumbers = +sumOfNumbers + +array[i];
+    }
+    return sumOfNumbers;
   } else {
     return 0;
   }
-  for (i in array) {
-    sumOfNumbers = +sumOfNumbers + +array[i];
-  }
-  return sumOfNumbers;
 };
 
-const multiply = function () {};
+const multiply = function (array) {
+  if (array.length) {
+    var result = 1;
+    for (i in array) {
+      result = result * +array[i];
+    }
+    return result;
+  } else {
+    return 0;
+  }
+};
 
-const power = function () {};
+const power = function (number, power) {
+  result = 1;
+  for (i = 1; i <= power; i++) {
+    result = result * number;
+  }
+  return result;
+};
 
-const factorial = function () {};
+const factorial = function (number) {
+  if (number === 0) {
+    return 1;
+  } else {
+    result = 1;
+    for (i = 1; i <= number; i++) {
+      result = result * i;
+    }
+    return result;
+  }
+};
 
 // Do not edit below this line
 module.exports = {
