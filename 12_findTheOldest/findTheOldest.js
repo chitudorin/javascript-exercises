881493440fc43c844105e3696dcc084110393c28
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-12 21:38:09 +0200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  let max = 0;
+  let maxAge = 0;
+  let age;
+  for (person of array) {
+    if (!person.yearOfDeath) {
+      const d = new Date();
+      age = d.getFullYear() - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+    if (age > maxAge) {
+      maxAge = age;
+      max = array.indexOf(person);
+    }
+  }
 
+  return array[max];
 };
 
 // Do not edit below this line
