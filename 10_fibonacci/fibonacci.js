b1b27bf832856e2c223bd53f7d3c8b95b40fb0c7
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-12 21:00:17 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (nth) {
+  if (nth < 0) {
+    return "OOPS";
+  } else {
+    let first = 0;
+    let second = 1;
+    let middleMan;
+    for (i = 0; i <= nth - 2; i++) {
+      middleMan = second;
+      second = first + second;
+      first = middleMan;
+    }
+    return second;
+  }
 };
 
 // Do not edit below this line
