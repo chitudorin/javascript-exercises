46ffd55269177a331ddeee45399878246b23a5d1
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-12 20:47:46 +0200

    Dismiss digits from regex replace on exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,7 @@ const palindromes = function (string) {
   check = true;
 
   //Remove punctuation or word breaks
-  noPunct = string.replace(/[^A-Za-z0-9]/g, "");
+  noPunct = string.replace(/[^A-Za-z]/g, "");
   string = noPunct.toLowerCase();
   for (i = 0; i <= string.length / 2; i++) {
     if (!(string[i] === string[string.length - i - 1])) {
