2313b28a56f3f6ab4ed2911b2e52435951f7e503
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-12 21:31:45 +0200

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  title = [];
+  for (i of books) {
+    title.push(i.title);
+  }
+  return title;
 };
 
 // Do not edit below this line
