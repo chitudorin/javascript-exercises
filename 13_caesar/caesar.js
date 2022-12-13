150bd114b0cfb5177129224a7508a840204d12cc
Chitu Dorin <46376664+chitudorin@users.noreply.github.com>
2022-12-13 06:48:23 +0200

    Complete exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,31 @@
-const caesar = function() {
-
+const caesar = function (string, shift) {
+  let caesaredChar;
+  let caesaredString = "";
+  for (i of string) {
+    charCode = i.charCodeAt(0);
+    if (charCode >= 65 && charCode <= 90) {
+      if (charCode + shift >= 90) {
+        charCode = 65 + (((charCode + shift) % 90) % 26) - 1;
+      } else if (charCode + shift <= 65) {
+        charCode = 90 - (65 % (charCode + shift)) + 1;
+      } else {
+        charCode = charCode + shift;
+      }
+    } else if (charCode >= 97 && charCode <= 122) {
+      if (charCode + shift >= 122) {
+        charCode = 97 + (((charCode + shift) % 122) % 26) - 1;
+      } else if (charCode + shift <= 97) {
+        charCode = 122 - (97 % (charCode + shift)) + 1;
+      } else {
+        charCode = charCode + shift;
+      }
+    }
+    caesaredChar = String.fromCharCode(charCode);
+    caesaredString += caesaredChar;
+  }
+  return caesaredString;
 };
 
 // Do not edit below this line
 module.exports = caesar;
+// if((charCode >= 65 && charCode <= 90)&&((charCode+ +shift <= 65 || charCode + +shift >= 90))
